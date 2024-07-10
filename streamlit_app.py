dc965d5261798f8ea5d831ff104806ae700aa8be
Snowflake-lab <naoufal.azizi@emglive.com>
2024-07-10 19:02:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ ingredients_list=st.multiselect(
     ,max_selections=5
 )
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#st.text(fruityvice_response.json())
 if ingredients_list:
    # st.write(ingredients_list)
     #st.text(ingredients_list)
